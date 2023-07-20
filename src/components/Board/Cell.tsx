18534d562968607bb911a30bfeb05ef90294114c
elproducer <elproducer20@gmail.com>
2023-07-20 08:04:11 -0700

    feat: init commit

---
+++ src/components/Board/Cell.tsx
@@ -0,0 +1,32 @@
+import styles from "./Board.module.scss";
+import {BoardNumberByLetter, Colors} from "../../types";
+import classNames from "classnames/bind";
+
+interface CellProps {
+  color: Colors;
+  x: string;
+  y: number;
+  cellClicked: (x: number, y: number) => void;
+  isAvailableForMove?: boolean;
+  isHavingFigure?: boolean;
+  isSelected?: boolean;
+}
+
+const Cell = (props: CellProps) => {
+  return (
+    <li onClick={() => props.cellClicked(BoardNumberByLetter[props.x], props.y)} id={`cell-${props.x}-${props.y}`} className={
+      classNames(styles.cell, {
+        [styles.cellWhite]: props.color === Colors.WHITE,
+        [styles.cellBlack]: props.color === Colors.BLACK,
+        [styles.availableCell]: props.isAvailableForMove && !props.isHavingFigure,
+        [styles.cellSelected]: props.isSelected,
+      })
+    }>
+      <div className={classNames(styles.cellCircle, {
+        [styles.cellCircleShow]: props.isAvailableForMove && !props.isHavingFigure
+      })}></div>
+    </li>
+  )
+}
+
+export  default Cell;
